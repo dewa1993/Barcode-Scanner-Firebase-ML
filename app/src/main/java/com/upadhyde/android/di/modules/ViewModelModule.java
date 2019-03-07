147c4555cd08081710c7b53b8de6e000011b142f
dewa1993 <Dewashish.upadhyay@gmail.com>
2019-03-07 12:39:51 +0530

    Base activity module

--- app/src/main/java/com/upadhyde/android/di/modules/ViewModelModule.java
+++ app/src/main/java/com/upadhyde/android/di/modules/ViewModelModule.java
@@ -1,9 +1,25 @@
 package com.upadhyde.android.di.modules;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.upadhyde.android.base.viewmodel.BaseViewModelFactory;
+import com.upadhyde.android.di.component.ViewModelKey;
+import com.upadhyde.android.viewmodel.main.DashboardFragmentViewModel;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
 @Module
-public class ViewModelModule {
+public abstract class ViewModelModule {
 
+    @Binds
+    abstract ViewModelProvider.Factory bindViewModelFactory(BaseViewModelFactory factory);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DashboardFragmentViewModel.class)
+    public abstract ViewModel bindDashboardFragmentViewModel(DashboardFragmentViewModel dashboardFragmentViewModel);
 
 }
